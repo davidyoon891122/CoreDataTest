1b8b28f389549525b9e9880b5689fb7713fe5396
davidyoon <qqpo12@naver.com>
2021-08-21 12:15:47 +0900

    set default settings for using CoreData

---
+++ CoreDataTest/Models/Todo.swift
@@ -0,0 +1,21 @@
+//
+//  Todo.swift
+//  CoreDataTest
+//
+//  Created by David Yoon on 2021/08/21.
+//
+
+import Foundation
+
+
+class Todo {
+    let title:String
+    let contents:String
+    
+    init(title: String, contents: String) {
+        self.title = title
+        self.contents = contents
+    }
+    
+    
+}
